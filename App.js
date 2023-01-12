98ccdab7fb16dabbec4cff2ef607b53f68aaf296
Carlos Cesar da Silva Ferraz Junior <carlos.cesar.ferraz@gmail.com>
2023-01-12 13:59:45 -0500

    useref working

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
+import { useState, useEffect, useRef } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [inputValue, setInputValue] = useState("");
+  const count = useRef(0);
+  const previousInputValue = useRef("");
+
+  useEffect(() => {
+    count.current = count.current + 1;
+  });
+
+  useEffect(() => {
+    previousInputValue.current = inputValue;
+  }, [inputValue]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        value={inputValue}
+        onChangeText={setInputValue}
+      />
+      <Text>Render Count: {count.current} </Text>
+      <Text>Input Value: {inputValue}</Text>
+      <Text>Previous Value: {previousInputValue.current}</Text>
     </View>
   );
 }
@@ -17,4 +37,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  }
 });
