1d03d64bba81fd41dab3e3b8aaf5b01b1348e03b
Carlos Cesar da Silva Ferraz Junior <carlos.cesar.ferraz@gmail.com>
2023-01-12 16:21:04 -0500

    Modifications

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { useReducer } from 'react';
+import BouncyCheckbox from "react-native-bouncy-checkbox";
+
+const initialTodos = [
+  {
+    id: 1,
+    title: "Todo 1",
+    complete: false,
+  },
+  {
+    id: 2,
+    title: "Todo 2",
+    complete: false,
+  },
+];
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "COMPLETE":
+      return state.map((todo) => {
+        if (todo.id === action.id) {
+          return { ...todo, complete: !todo.complete };
+        } else {
+          return todo;
+        }
+      });
+    default:
+      return state;
+  }
+};
 
 export default function App() {
+
+  const [todos, dispatch] = useReducer(reducer, initialTodos);
+
+  const handleComplete = (todo) => {
+    dispatch({ type: "COMPLETE", id: todo.id });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {todos.map((todo) => (
+        <View key={todo.id}>
+            <BouncyCheckbox
+              isChecked={todo.complete}
+              onPress={() => handleComplete(todo)}
+              text={todo.title}
+            />
+        </View>
+      ))}
     </View>
   );
 }
